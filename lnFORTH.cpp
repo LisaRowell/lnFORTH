c9698a4f1bf57d0bb7bf27f492cd29649328a00c
Lisa Rowell <lisa.a.rowell@me.com>
2026-07-23 15:07:15 -0700

    Added lost file header and moved stack initialization to the virtual machine initialiazation instead of hiding it in dictionary building.

--- lnFORTH.cpp
+++ lnFORTH.cpp
@@ -1,3 +1,21 @@
+/*
+ * lnFORTH - A portable token threaded figFORTH implementation.
+ * Copyright (C) 2026 Lisa Rowell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
@@ -3436,6 +3454,9 @@ void InitUserMemory() {
 }
 
 void InitVirtualMachine() {
+    sp = cellMax;
+    rp = cellMax;
+
     cell_t cold = ByteIndexToCellIndex(memory.cell[ByteIndexToCellIndex(ORIG)]) + 1;
     ip = cold;
 }
@@ -3467,8 +3488,6 @@ void RunVirtualMachine() {
 }
 
 void BuildDictionary() {
-    sp = cellMax;
-    rp = cellMax;
     here = ByteIndexToCellIndex(ORIG);
 
     if (debugStartup) {
